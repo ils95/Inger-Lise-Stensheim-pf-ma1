5d29b9413a84ac5d44ff61c546cf0be8d49d23ea
ils95 <63651881+ils95@users.noreply.github.com>
2020-05-25 15:53:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,41 @@
+// question 1
+
+var firstVar;
+console.log(firstVar);
+
+// question 2
+
+var name = "Ingerlise";
+console.log(name);
+
+// question 3
+
+var integer = 8;
+
+// question 4
+
+var division = 20 / 5;
+console.log(division);
+
+// question 5
+
+console.log(typeof "frog");
+
+// question 6
+
+var orderHasShipped = true;
+console.log(orderHasShipped);
+
+// question 7
+
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+
+// question 8
+
+for (var count = 1; count <= 9; count++) {
+  console.log(count);
+}
